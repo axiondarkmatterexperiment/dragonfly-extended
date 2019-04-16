27bf55e3e1b55b6e931d4ea8e6bf06d5f8a7cd44
Ben LaRoque <benjamin.laroque@gmail.com>
2019-04-16 15:29:28 -0700

    adding non-dripline script to remove egg files

---
+++ source/egg_rm.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import datetime
+import glob
+import time
+import os
+
+def do_cleanup(num_to_keep, glob_arg):
+    files_list = glob.glob(glob_arg)
+    files_list.sort(key=os.path.getmtime)
+    for a_file in files_list[:-num_to_keep]:
+        print("[{}]: removing file '{}' (not among {} newest)".format(datetime.datetime.now(), a_file, num_to_keep))
+        os.remove(a_file)
+
+if __name__ == '__main__':
+    num_to_keep = 10
+    glob_arg = '/data/*.egg'
+    while True:
+        do_cleanup(num_to_keep, glob_arg)
+        time.sleep(20)
