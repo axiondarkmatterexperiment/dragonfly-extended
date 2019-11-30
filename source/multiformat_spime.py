55731094a51bc68c8d67d198fe40c2a12e69e370
Raphael Cervantes <raphaelc@uw.edu>
2019-11-29 20:59:07 -0800

    Added comments. Reformatted for PEP8

--- source/multiformat_spime.py
+++ source/multiformat_spime.py
@@ -1,5 +1,4 @@
 import math
-import sys
 import yaml
 import numpy as np
 import cmath
@@ -22,22 +21,26 @@ _all_calibrations = []
 
 def iq_packed2powers(iq_data):
     """Turn iq data in [r,i,r,i,r,i...] format into an array of powers"""
-    powers=np.zeros(int(len(iq_data)/2))
+    powers = np.zeros(int(len(iq_data)/2))
     for i in range(int(len(iq_data)/2)):
-        powers[i]=iq_data[2*i]*iq_data[2*i]+iq_data[2*i+1]*iq_data[2*i+1]
+        powers[i] = iq_data[2*i]*iq_data[2*i]+iq_data[2*i+1]*iq_data[2*i+1]
     return powers
 
+
 def unpack_iq_data(iq_data):
-    """takes iq data in [r,i,r,i,r,i] format and unpacks into two arrays [r,r,r],[i,i,i]"""
-    ret_r=np.zeros(len(iq_data)//2)
-    ret_i=np.zeros(len(iq_data)//2)
-    for i in range(0, len(iq_data),2):
-        ret_r[i//2]=iq_data[i]
-        ret_i[i//2]=iq_data[i+1]
-    return ret_r,ret_i
+    """takes iq data in [r,i,r,i,r,i] format and unpacks into two
+    arrays [r,r,r],[i,i,i]"""
+    ret_r = np.zeros(len(iq_data)//2)
+    ret_i = np.zeros(len(iq_data)//2)
+    for i in range(0, len(iq_data), 2):
+        ret_r[i//2] = iq_data[i]
+        ret_i[i//2] = iq_data[i+1]
+    return ret_r, ret_i
+
 
 def repack_iq_data(data_r, data_i):
-    """takes iq data seperated into 2 arrays [r,r,r], [i,i,i] and combines them into one array [r,i,r,i,r,i]. This method was made to test certain functions that takes iq data with data that had already been unpacked."""
+    """takes iq data seperated into 2 arrays [r,r,r], [i,i,i] and
+    combines them into one array [r,i,r,i,r,i]."""
     if not (data_r.size == data_i.size):
         raise ValueError("Real and imaginary vectors should be the same length")
     iq_series = np.empty((data_r.size + data_i.size,), dtype=data_r.dtype)
@@ -45,44 +48,64 @@ def repack_iq_data(data_r, data_i):
     iq_series[1::2] = data_i
     return iq_series
 
-def find_nearest_idx(array,value):
+
+def find_nearest_idx(array, value):
+    """Finds the index of an array element that is closest to specified
+    value"""
+    # TODO figure out what to do with symmetric functions.
     idx = (np.abs(array-value)).argmin()
     return idx
-def guess_fo(f, gamma2):
-    ind_fo = np.argmin(gamma2) #find index of resonant frequency
+
+
+def guess_fo(f, Gamma2):
+    """Guesses the resonant frequency"""
+    ind_fo = np.argmin(Gamma2)  # find index of resonant frequency
     return f[ind_fo]
 
+
 def guess_offset(y):
+    """Gueses normalization for reflection fit."""
     low_filt_perc = 0.33
-    y_filtered = stats.trim1(y, low_filt_perc, tail = 'left') #cut out bottom low_filt_perc of y values. Basically want to filter out the notch a bit.
+    # cut out bottom low_filt_perc of y values. Basically want to filter
+    # out the notch a bit.
+    y_filtered = stats.trim1(y, low_filt_perc, tail='left')
     return np.median(y_filtered)
 
+
 def guess_dy(y):
-    return guess_offset(y) - np.min(y) 
+    """Returns a guess for the depth of the Lorentzian"""
+    return guess_offset(y) - np.min(y)
+
 
 def guess_q(f, y):
-    ind_fc = np.argmin(y) #find index of resonant frequency
-    fc = f[ind_fc] #obtain resonant frequency
+    """Returns a guess for the Q of the Lorentzian"""
+    ind_fc = np.argmin(y)  # find index of resonant frequency
+    fc = f[ind_fc]  # obtain resonant frequency
 
-    #look at the left of the resonance
-    left_f = f[:ind_fc] 
-    left_y = y[:ind_fc] 
+    # look at the left of the resonance
+    left_f = f[:ind_fc]
+    left_y = y[:ind_fc]
     dy = guess_dy(y)
-    ind_fwhm = find_nearest_idx(left_y, dy/2)
+    C = guess_offset(y)
+    ind_fwhm = find_nearest_idx(left_y, C-dy/2)
 
-    #find distance between fwhm and resonance
+    # find distance between fwhm and resonance
     f1 = f[ind_fwhm]
-    #guess bandwidth as twice that distance
+    # guess bandwidth as twice that distance
     del_f = 2*(fc-f1)
     Q_guess = fc/del_f
     return Q_guess
 
-def guess_reflection_fit_params(f, gamma2):
-    fo_guess = guess_fo(f, gamma2)
-    Q_guess = guess_q(f, gamma2)
-    dy_guess = guess_dy(gamma2)
-    C_guess = guess_offset(gamma2)
+
+def guess_reflection_fit_params(f, Gamma2):
+    """Finds an initial guess for the fittings parameters of reflected
+    power"""
+    fo_guess = guess_fo(f, Gamma2)
+    Q_guess = guess_q(f, Gamma2)
+    dy_guess = guess_dy(Gamma2)
+    C_guess = guess_offset(Gamma2)
     return fo_guess, Q_guess, dy_guess, C_guess
+
 
 def func_pow_reflected(f, fo, Q, del_y, C):
     """The reflected power. Just a Lorentzian"""
@@ -90,49 +113,61 @@ def func_pow_reflected(f, fo, Q, del_y, C):
 
 
 def get_arr_ends(x, n_end_elements):
-    return np.concatenate([x[:n_end_elements],x[-n_end_elements:]])
+    """returns the first and last n_end_elements of array x"""
+    return np.concatenate([x[:n_end_elements], x[-n_end_elements:]])
 
-def deconvolve_transmission(f, gamma_mag, gamma_phase, C_fit):
-    gamma_cav_mag = gamma_mag*np.sqrt(1/C_fit)
 
-    interp_phase = interp1d(f, gamma_phase, kind='cubic')
+def deconvolve_transmission(f, Gamma_mag, Gamma_phase, C_fit):
+    """Finds the reflection coefficient off of the cavity by deconvolving
+    the """
+    Gamma_cav_mag = Gamma_mag*np.sqrt(1/C_fit)
+
+    interp_phase = interp1d(f, Gamma_phase, kind='cubic')
     f_ends = get_arr_ends(f, 5)
-    phase_ends = get_arr_ends(gamma_phase, 5)
-#    interp_phase_wo_notch = interp1d(f_ends, phase_ends, kind='linear')
+    phase_ends = get_arr_ends(Gamma_phase, 5)
     interp_phase_wo_notch = np.poly1d(np.polyfit(f_ends, phase_ends, 1))
     delay_phase = interp_phase_wo_notch(f)
-    gamma_cav_phase = interp_phase(f) - delay_phase
+    Gamma_cav_phase = interp_phase(f) - delay_phase
 
-    return gamma_cav_mag, gamma_cav_phase
+    return Gamma_cav_mag, Gamma_cav_phase
 
 
-def calculate_coupling(gamma_mag_fo, gamma_phase_fo):
+def calculate_coupling(mag_fo, phase_fo):
     """Calculate coupling to a cavity after reflection fit is done"""
-    beta = (1+np.sign(gamma_phase_fo - np.pi)*np.abs(gamma_mag_fo))/(1-np.sign(gamma_phase_fo - np.pi)*np.abs(gamma_mag_fo))
+    sgn = np.sign(phase_fo - np.pi)
+    beta = (1+sgn*mag_fo)/(1-sgn*mag_fo)
     return beta
 
+
 def estimate_power_uncertainty(power):
-    """Assume fractional uncertainty in power is constant. This is not a correct assumption, but probably good enough for now."""
-    pow_fractional_std = np.std(get_arr_ends(power, 5))/np.mean(get_arr_ends(power, 5))
+    """Assume relative uncertainty in power is constant. This is not
+    a correct assumption, but probably good enough for now."""
+    pow_ends = get_arr_ends(power, 5)
+    rel_sig_pow = np.std(pow_ends)/np.mean(pow_ends)
     sig_power = power*pow_fractional_std
     return sig_power
 
+
 def calc_red_chisq(x, y, sigma_y, func, fit_param):
+    """Calculates the reduced chi-square"""
     resid = y-func(x, *fit_param)
     chisq = np.sum(resid/sigma_y)
     dof = len(y)-len(fit_param)
     red_chisq = chisq/dof
     return red_chisq
 
-def fit_shape_database_hack(x, func, fit_param):
-    """This takes the sidecar fit, which is in power, and returns it into the fit shape that"""
-    gamma_mag = np.sqrt(func(x, *fit_param))
 
-    gamma_dummy = np.zeros_like(gamma_mag)
-    fit_shape = repack_iq_data(gamma_mag, gamma_dummy)
+def fit_shape_database_hack(x, func, fit_param):
+    """This takes the sidecar fit, which is in power, and returns it into the
+    fit shape that the database expects. It expects iq series [r,i,r,i....]"""
+    Gamma_mag = np.sqrt(func(x, *fit_param))
+
+    Gamma_dummy = np.zeros_like(Gamma_mag)
+    fit_shape = repack_iq_data(Gamma_mag, Gamma_dummy)
     return fit_shape
 
-def transmission_power_shape(f,norm,f0,Q,noise):
+
+def transmission_power_shape(f, norm, f0, Q, noise):
     """returns the expected power from a transmission measurement at frequency f with parameters
         f0 - resonant frequency
         Q - resonant quality
@@ -140,6 +175,7 @@ def transmission_power_shape(f,norm,f0,Q,noise):
         noise - estimate of the noise background"""
     delta=Q*(f-f0)/f0
     return norm*(1/(1+4*delta*delta)+noise)
+
 
 def reflection_iq_shape(f,norm,phase,f0,Q,beta,delay_time):
     """returns the expected [i,q] values from a reflection measurement
@@ -315,61 +351,76 @@ def fit_reflection(iq_data,frequencies):
     #return norm,phase,f0,Q,beta,delay_time,chi-square of fit
     return [res.x[0],res.x[1],res.x[2],res.x[3],res.x[4],res.x[5],chisq,fit_shape,dip_depth]
 
-def sidecar_fit_reflection(iq_data,frequencies):
-    """fits sidecar reflection data. For now, it is separate function from the main experiment so as not to disturb it."""
-    #TODO Estimate uncertainty appropriately
 
+def sidecar_fit_reflection(iq_data, frequencies):
+    """fits sidecar reflection data. For now, it is separate function from 
+    the main experiment so as not to disturb it.
+    Gamma is the measured reflection coefficient"""
+    # TODO Estimate uncertainty appropriately
+
+    # TODO change powers to an existing variable
     if 2*len(frequencies)!=len(iq_data):
-        raise ValueError("point count not right nfreqs {} npows {}".format(len(frequencies),len(powers)))
-    if len(frequencies)<16:
+        raise ValueError("point count not right nfreqs {} npows {}".format(len(frequencies), len(powers)))
+    if len(frequencies) < 16:
         raise ValueError("not enough points to fit transmission, need 16, got {}".format(len(powers)))
 
+    Gamma_r, Gamma_i = unpack_iq_data(iq_data)
+    Gamma_complex = Gamma_r+Gamma_i*1j
+    Gamma_mag_sq = Gamma_r**2 + Gamma_i**2
+    sig_Gamma_mag_sq = estimate_power_uncertainty(Gamma_mag_sq)
+    Gamma_mag = np.sqrt(Gamma_mag_sq)
+    Gamma_phase = np.unwrap(np.angle(Gamma_complex))
 
-    gamma_r, gamma_i = unpack_iq_data(iq_data)
-    gamma_complex = gamma_r+gamma_i*1j
-    gamma_mag_sq= gamma_r**2 + gamma_i**2
-    sig_gamma_mag_sq = estimate_power_uncertainty(gamma_mag_sq)
-    gamma_mag = np.sqrt(gamma_mag_sq)
-    gamma_phase = np.unwrap(np.angle(gamma_complex))
+    po_guess = guess_reflection_fit_params(frequencies, Gamma_mag_sq)
 
-    po_guess = guess_reflection_fit_params(frequencies, gamma_mag_sq)
-
-    pow_fit_param, pow_fit_cov = curve_fit(func_pow_reflected, frequencies, 
-                                           gamma_mag_sq, p0=po_guess, sigma= sig_gamma_mag_sq)
+    pow_fit_param, pow_fit_cov = curve_fit(func_pow_reflected, frequencies,
+                                           Gamma_mag_sq, p0=po_guess,
+                                           sigma=sig_Gamma_mag_sq)
 
     fo_fit, Q_fit, del_y_fit, C_fit = pow_fit_param
 
-    red_chisq = calc_red_chisq(frequencies, gamma_mag_sq, sig_gamma_mag_sq, func_pow_reflected, pow_fit_param)
+    red_chisq = calc_red_chisq(frequencies, Gamma_mag_sq, sig_Gamma_mag_sq,
+                               func_pow_reflected, pow_fit_param)
 
+    # Gam_c is reflection coeffient Gamma of the cavity
+    Gam_c_mag, Gam_c_phase = deconvolve_transmission(frequencies, Gamma_mag,
+                                                     Gamma_phase, C_fit)
 
-    gamma_cav_mag, gamma_cav_phase = deconvolve_transmission(frequencies, gamma_mag, gamma_phase, C_fit) #this is from data, not from the fitted function.
-
-    gamma_cav_mag_fo_from_fit = np.sqrt(func_pow_reflected(fo_fit, *pow_fit_param)*1/C_fit)
+    # Calculates magnitude of Gamma_cavity by plugging resonant frequency into
+    # fitted function
+    Gam_c_mag_fo = np.sqrt(func_pow_reflected(fo_fit, *pow_fit_param)*1/C_fit)
     
-    interp_phase = interp1d(frequencies, gamma_cav_phase, kind='cubic')
+    Gam_c_interp_phase = interp1d(frequencies, Gam_c_phase, kind='cubic')
 
-    gamma_cav_phase_fo_from_interp = interp_phase(fo_fit)
+    # calculate phase of Gamma_cavity at resonant frequency by interpolating
+    # data.
+    Gam_c_phase_fo = Gam_c_interp_phase(fo_fit)
     
-    beta = calculate_coupling(gamma_cav_mag_fo_from_fit, gamma_cav_phase_fo_from_interp)
+    beta = calculate_coupling(Gam_c_mag_fo, Gam_c_phase_fo)
     
-    delay_time = -1
-    fit_shape = fit_shape_database_hack(frequencies, func_pow_reflected, pow_fit_param)
+    # I don't get a delay time through this analysis. Just setting to -1 so I 
+    # can match Gray's database.
+    delay_time = -1 
+
+    fit_shape = fit_shape_database_hack(frequencies, func_pow_reflected,
+                                        pow_fit_param)
 
     logger.info("norm {}".format(C_fit))
-    logger.info("phase {}".format(gamma_cav_phase_fo_from_interp))
+    logger.info("phase {}".format(Gam_c_phase_fo))
     logger.info("f0 fit {}".format(fo_fit))
     logger.info("Q fit {}".format(Q_fit))
     logger.info("beta fit {}".format(beta))
-    logger.info("delay time fit {}".format(delay_time))
     logger.info("reduced chi-square {}".format(red_chisq))
-    logger.info("fit shape {}".format(fit_shape))
-    logger.info("dip dept {}".format(del_y_fit))
+    logger.info("dip depth {}".format(del_y_fit))
 
-    #turn numpy arrays to lists so that json can iterate through it.
-    gamma_cav_phase_fo_from_interp = gamma_cav_phase_fo_from_interp.tolist() #apparently, json can't deal with numpy objects, even if they are just a single number. I don't know.
+    # turn numpy arrays to lists so that json can iterate through it.
+    # apparently, json can't deal with numpy objects, even if they are just a
+    # single number. I don't know.
+    Gam_c_phase_fo_from_interp = Gam_c_phase_fo.tolist() 
     fit_shape = fit_shape.tolist()
 
-    return [C_fit, gamma_cav_phase_fo_from_interp, fo_fit, Q_fit, beta, delay_time, red_chisq, fit_shape, del_y_fit]
+    return [C_fit, Gam_c_phase_fo_from_interp, fo_fit, Q_fit, beta,
+            delay_time, red_chisq, fit_shape, del_y_fit]
 
 
 def semicolon_array_to_json_object(data_string,label_array):
@@ -463,6 +514,7 @@ def reflection_calibration(data_object):
     return data_object
 _all_calibrations.append(reflection_calibration)
 
+
 def sidecar_reflection_calibration(data_object):
     """takes a network analyzer output of format 
             {
@@ -470,7 +522,7 @@ def sidecar_reflection_calibration(data_object):
         stop_frequency: <number>
         iq_data: <array of numbers, packed i,r,i,r>
             }
-        and augments it with a transmission fit
+        and augments it with a reflection fit
           {
         fit_f0: <number>
         fit_Q: <number>
@@ -479,18 +531,20 @@ def sidecar_reflection_calibration(data_object):
         fit_chisq: <number>
           }
     """
-    freqs=np.linspace(data_object["start_frequency"],data_object["stop_frequency"],int(len(data_object["iq_data"])/2))
-    logger.info("Help 0")
-    fit_norm, fit_phase, fit_f0, fit_Q, fit_beta, fit_delay_time, fit_chisq, fit_shape, dip_depth = sidecar_fit_reflection(data_object["iq_data"],freqs)
-    data_object["fit_norm"]=fit_norm
-    data_object["fit_phase"]=fit_phase
-    data_object["fit_f0"]=fit_f0
-    data_object["fit_Q"]=fit_Q
-    data_object["fit_beta"]=fit_beta
-    data_object["fit_delay_time"]=fit_delay_time
-    data_object["fit_chisq"]=fit_chisq
-    data_object["fit_shape"]=fit_shape
-    data_object["dip_depth"]=dip_depth
+    freqs = np.linspace(data_object["start_frequency"],
+                        data_object["stop_frequency"],
+                        int(len(data_object["iq_data"])/2))
+
+    fit_output = sidecar_fit_reflection(data_object["iq_data"], freqs)
+    data_object["fit_norm"] = fit_output[0]
+    data_object["fit_phase"] = fit_output[1]
+    data_object["fit_f0"] = fit_output[2]
+    data_object["fit_Q"] = fit_output[3]
+    data_object["fit_beta"] = fit_output[4]
+    data_object["fit_delay_time"] = fit_output[5]
+    data_object["fit_chisq"] = fit_output[6]
+    data_object["fit_shape"] = fit_output[7]
+    data_object["dip_depth"] = fit_output[8]
     return data_object
 _all_calibrations.append(sidecar_reflection_calibration)
 
