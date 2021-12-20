68c7ac129195a9bab770f1c6a6f73c74cb70131b
Erik Lentz <lentzew@users.noreply.github.com>
2021-12-20 11:58:31 -0800

    Update ethernet_provider_waveform.py

--- source/ethernet_provider_waveform.py
+++ source/ethernet_provider_waveform.py
@@ -13,55 +13,17 @@ class EthernetProviderWaveform(EthernetProvider):
         EthernetProvider.__init__(self, **kwargs)
         return None
     
-    def send_waveform(self,**parameters):
+    def send_waveform(self,commands,**parameters):
         '''
-       Takes the waveform endpoint set strings and 
-       Restructure of 'send' in Ethernetprovider
+       Takes list of endpoint command strings and concatenates them before send as single message
+       For use w/ waveform endpoints to be passed to arbitrary waveform generator
         '''
         # collect waveform writing endpoints, combine, and consolidate commands 
-
-
-        # execute send from EthernetProvider        
+        waveform_write_command = ''
+        for command in commands:
+            waveform_write_command += command
+        # execute send from EthernetProvider      
+        self.send(waveform_write_command, **parameters)
         return None
-    
-    def send_waveform(self, commands, **kwargs):
-        '''
-        Takes the waveform endpoint set strings and 
-        Restructure of 'send' in Ethernetprovider
-        
-        Standard provider method to communicate with instrument.
-        NEVER RENAME THIS METHOD!
-        commands (list||None): list of command(s) to send to the instrument following (re)connection to the instrument, still must return a reply!
-                             : if impossible, set as None to skip
-        '''
-        if isinstance(commands, six.string_types):
-            commands = [commands]
-        self.alock.acquire()
-
-        try:
-            data = self._send_commands(commands)
-        except socket.error as err:
-            logger.warning("socket.error <{}> received, attempting reconnect".format(err))
-            self._reconnect()
-            data = self._send_commands(commands)
-            logger.critical("Ethernet connection reestablished")
-        except exceptions.DriplineHardwareResponselessError as err:
-            logger.critical(str(err))
-            try:
-                self._reconnect()
-                data = self._send_commands(commands)
-                logger.critical("Query successful after ethernet connection recovered")
-            except exceptions.DriplineHardwareConnectionError:
-                logger.critical("Ethernet reconnect failed, dead socket")
-                raise exceptions.DriplineHardwareConnectionError("Broken ethernet socket")
-            except exceptions.DriplineHardwareResponselessError as err:
-                logger.critical("Query failed after successful ethernet socket reconnect")
-                raise exceptions.DriplineHardwareResponselessError(err)
-        finally:
-            self.alock.release()
-        to_return = ';'.join(data)
-        logger.debug("should return:\n{}".format(to_return))
-        return to_return
-    
     
     pass
