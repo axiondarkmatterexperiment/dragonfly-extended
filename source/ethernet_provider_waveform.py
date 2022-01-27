86d3c4f4dc74fa09ac257a9b7dcea0fa3cafc2c0
Erik Lentz <lentzew@users.noreply.github.com>
2022-01-26 18:14:49 -0800

    Update ethernet_provider_waveform.py
    added methods to handle endpoints with partial messages, for use with waveform updates

--- source/ethernet_provider_waveform.py
+++ source/ethernet_provider_waveform.py
@@ -1,4 +1,6 @@
 import dripline
+import scarab
+from dripline.core import op_t, MsgRequest, DriplineError
 from dragonfly.implementations import EthernetProvider
 
 import logging
@@ -15,6 +17,33 @@ class EthernetProviderWaveform(EthernetProvider):
         '''
         EthernetProvider.__init__(self, **kwargs)
         return None
+    
+    def set_partial(self, endpoint, value, specifier=None, timeout=0):
+        '''
+        method for setting partial messages
+        
+        [kw]args:
+        endpoint (string): routing key to which an OP_GET will be sent
+        value : value to assign
+        specifier (string|None): specifier to add to the message
+        timeout (float|int): time to timeout, in seconds
+        '''
+        payload = {'values':[value]}
+        result = self._generate_request_partial( msgop=op_t.set, target=endpoint, specifier=specifier, payload=payload )
+        reply = self._receiver.wait_for_reply(result, timeout) # not sure how crucial the role of this step is
+        return result
+    
+    def _generate_request_partial(self, msgop, target, specifier=None, payload=None, timeout=None, lockout_key=False):
+        '''
+        internal helper method to standardize generating requests for partial messages
+        returns (partial) message request
+        '''
+        a_specifier = specifier if specifier is not None else ""
+        a_request = MsgRequest.create(payload=scarab.to_param(payload), msg_op=msgop, routing_key=target, specifier=a_specifier)
+        #receive_reply = self.send(a_request)
+        #if not receive_reply.successful_send:
+        #    raise DriplineError('unable to send request')
+        return a_request
     
     def send_waveform(self,commands,**parameters):
         '''
