1c40f2402098ce4d932b411d59fb0b04e08e3115
Dan Zhang <danavagor@gmail.com>
2023-10-17 16:00:12 -0700

    debug

--- source/multiformat_spime.py
+++ source/multiformat_spime.py
@@ -346,8 +346,8 @@ def fit_reflection(iq_data,frequencies):
     
     #calculate shape
     fit_shape = fit_fcn(frequencies,par[0],par[1],par[2],par[3],par[4],par[5]) 
-    return np.append(par,[0,0,0]) 
     chisq=np.power((fit_shape-iq_data)/uncertainty,2)/len(frequencies)
+    return np.append(par,[chisq,0,dip_depth]) 
     #TODO at this point change to dict
     #return norm,phase,f0,Q,beta,delay_time,chi-square of fit
     return [par[0],par[1],par[2],par[3],par[4],par[5],chisq,fit_shape,dip_depth]
